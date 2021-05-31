15f1d0333a3b4fa2e49842c1d043766589ee9d4c
Raul Vallespin <rvallespin@kidodynamics.com>
2021-05-31 09:15:51 +0200

    Updates

---
+++ precache-manifest.dbd123119251a37a490e3e7124017b27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1766d4f4d9f9a310fa1e86d0f363670a",
+    "url": "/snapshot/index.html"
+  },
+  {
+    "revision": "6cd2a45dfe78c7c2a8ad",
+    "url": "/snapshot/static/css/main.9b556514.chunk.css"
+  },
+  {
+    "revision": "f700a161556c5079a10c",
+    "url": "/snapshot/static/js/2.ce1f4c8d.chunk.js"
+  },
+  {
+    "revision": "aa9edb028f3cc5563024a0374bb27b3d",
+    "url": "/snapshot/static/js/2.ce1f4c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd2a45dfe78c7c2a8ad",
+    "url": "/snapshot/static/js/main.c0709c45.chunk.js"
+  },
+  {
+    "revision": "b925947df042ea077e2e1d454b2ce30c",
+    "url": "/snapshot/static/js/mapbox-gl-csp-worker.bb9e1d88.worker.js"
+  },
+  {
+    "revision": "8ff9862a264c3de9b59d",
+    "url": "/snapshot/static/js/runtime-main.5e74a6cc.js"
+  }
+]);
